3adbb8899c87664d78771452aabe488c37d81d6e
aleferu <aleferu888@gmail.com>
2023-09-06 20:32:10 +0200

    Turtle graphics progress, nothing tested so maybe wrong

--- canvas.py
+++ canvas.py
@@ -1,11 +1,13 @@
-import sys
-
 
 class TurtleCanvas():
-    def __init__(self, w, h, bg_color):
+    def __init__(self, w, h, bg_color=0x000000, pen_color=0xFFFFFF):
         self.width = w
         self.height = h
         self.pixels = [bg_color] * (w * h)
+        self.cursor = 0
+        self.pen_down = True
+        self.angle = 0
+        self.color = pen_color
 
     def save_to_ppm_file(self, name):
         with open(name, 'w') as f:
@@ -16,8 +18,36 @@ class TurtleCanvas():
                 b = pixel >> (8 * 0) & 0xFF
                 f.write(f'{r} {g} {b}\n')
 
-    def draw_pixel(self, x, y, color):
+    def up(self):
+        self.is_pen_down = False
+
+    def down(self):
+        self.is_pen_down = True
+
+    def right(self, turning_angle):
+        self.angle += turning_angle
+
+    def left(self, turning_angle):
+        self.angle -= turning_angle
+
+    def forward(self, steps):
+        # TODO: implement DDA algorithm?
+        pass
+
+    def backwards(self, steps):
+        # TODO: Maybe self.forward(-steps) would work
+        pass
+
+    def set_color(self, color):
+        self.color = color
+
+    def goto(self, x, y):
         if x >= self.width or y >= self.height:
-            print(f'{x} {y} not in {self.width}x{self.height}')
+            print(f'Tried to call goto({x}, {y}) ')
+            print(f'but the canvas is {self.width}x{self.height}')
             return
-        self.pixels[x * self.width + y] = color
+        self.cursor = x * self.width + y
+
+    def dot(self):
+        if self.is_pen_down:
+            self.pixels[self.cursor] = self.color
--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from canvas import TurtleCanvas
 
 
 def main():
-    canvas = TurtleCanvas(2, 2, 0x000000)
-    canvas.draw_pixel(0, 1, 0xff0000)
-    canvas.save_to_ppm_file('test.ppm')
+    # TODO
+    pass
 
 
 if __name__ == '__main__':
